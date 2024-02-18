1ad32614dc88e7aa254f20b9fec806698e50ed84
Istiyak <istiyak.riyad@gmail.com>
2024-02-19 02:54:21 +0600

    Adding more functionality to go template

---
+++ 7. template-2/README.md
@@ -0,0 +1,12 @@
+# Go - Template Standard Library
+
+Html template custom function generate and use cases.
+
+## Tutorial Links
+* [Go Template Use Case](https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-go)
+
+
+## Usefull Links
+* [Go HTML Template std package](https://pkg.go.dev/html/template)
+* [Go Text Template std package](https://pkg.go.dev/text/template)
+
--- 7. template-2/main.go
+++ 7. template-2/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"os"
+	"strings"
 )
 
 
@@ -11,7 +13,63 @@ type Pet struct {
 	Sex		string
 	Intact	bool
 	Age		string
-	Breed	string
+	Breed	[]string
+}
+
+
+func arrayAndOtherFunctions(dogs []Pet) {
+	tmplFile := "pets.tmpl"
+	tmpl, err := template.New(tmplFile).ParseFiles(tmplFile)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hello world")
+
+	if err := tmpl.Execute(os.Stdout, dogs); err != nil {
+		panic(err)
+	}
+}
+
+func customFunc(dogs []Pet) {
+	funcMap := template.FuncMap{
+		"dec" : func(i int) int { return i - 1 },
+		"replace": strings.ReplaceAll,
+		"join": strings.Join,
+	}
+
+	fileName := "lastPet.tmpl"
+	tmpl, err := template.New(fileName).Funcs(funcMap).ParseFiles(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tmpl.Execute(os.Stdout, dogs); err != nil {
+		panic(err)
+	}
+}
+
+func htmlRendering(dogs []Pet) {
+	funcMap := template.FuncMap{
+		"dec" : func(i int) int { return i - 1 },
+		"replace": strings.ReplaceAll,
+		"join": strings.Join,
+	}
+
+	fileName := "petsHtml.tmpl"
+	tmpl, err := template.New(fileName).Funcs(funcMap).ParseFiles(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	// Write the html file
+	file, err := os.Create("pets.html")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tmpl.Execute(file, dogs); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
@@ -21,33 +79,27 @@ func main() {
 			Sex:    "Female",
 			Intact: false,
 			Age:    "10 months",
-			Breed:  "German Shepherd/Pitbull",
+			Breed:  []string{"German Shepherd", "Pitbull"},
 		},
 		{
 			Name:   "Zephyr",
 			Sex:    "Male",
 			Intact: true,
 			Age:    "13 years, 3 months",
-			Breed:  "German Shepherd/Border Collie",
+			Breed:  []string{"German Shepherd", "Border Collie"},
 		},
 		{
 			Name:	"Bruce Wayne",
 			Sex:	"Male",
 			Intact:	false,
 			Age:	"3 years, 8 months",
-			Breed:	"Chihuahua",
+			Breed:	[]string{"Chihuahua"},
 		},
 	}
-
-	tmplFile := "pets.tmpl"
-	tmpl, err := template.New(tmplFile).ParseFiles(tmplFile)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := tmpl.Execute(os.Stdout, dogs); err != nil {
-		panic(err)
-	}
+	
+	// arrayAndOtherFunctions(dogs)
+	// customFunc(dogs)
+	htmlRendering(dogs)
 }
 
 
