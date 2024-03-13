2fa776431506f094055aec6095ceb5346867b706
Istiyak <istiyak.riyad@gmail.com>
2024-03-13 17:44:17 +0600

    Adding more context

--- 9. context/main.go
+++ 9. context/main.go
@@ -6,18 +6,45 @@ import (
 	"time"
 )
 
-func doSomething(ctx context.Context) {
-	fmt.Println(time.Now())
+func doAnother(ctx context.Context, printCh <- chan int) {
+	for {
+		select {
+		case <- ctx.Done() : 
+			if err := ctx.Err() ; err != nil {
+				fmt.Printf("do Another error: %s\n", err)
+			}
+			fmt.Println("do Another finished")
+			return;
 
-	fmt.Printf("Value of myKey : %s\n", ctx.Value("myKey"))
-	doAnotherCtx := context.WithValue(ctx, "myKey", "AnotherValue")
-
-	doAnother(doAnotherCtx)
-	fmt.Printf("Value of myKey : %s\n", ctx.Value("myKey"))
+		case num := <- printCh :
+			fmt.Println("do Another ", num)
+		}
+	}
+//	fmt.Printf("The do another function, value : %s\n", ctx.Value("myKey"))
 }
 
-func doAnother(ctx context.Context) {
-	fmt.Printf("The do another function, value : %s\n", ctx.Value("myKey"))
+func doSomething(ctx context.Context) {
+	ctx, cancelCtx := context.WithCancel(ctx)
+
+	printCh := make(chan int)
+	go doAnother(ctx, printCh)
+
+	for i := 1; i <= 3; i++ {
+		printCh <- i
+	}
+
+	cancelCtx()
+	time.Sleep(300 * time.Millisecond)
+
+	fmt.Println("Do something finished")
+
+//	fmt.Println(time.Now())
+//
+//	fmt.Printf("Value of myKey : %s\n", ctx.Value("myKey"))
+//	doAnotherCtx := context.WithValue(ctx, "myKey", "AnotherValue")
+//
+//	doAnother(doAnotherCtx)
+//	fmt.Printf("Value of myKey : %s\n", ctx.Value("myKey"))
 }
 
 func main() {
